6b4ac91c0b4ac31a57ead75a67921341fc5bf101
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-08-31 14:51:30 +0200

    Add validation and avoid invoking getters/setters (#14)

--- binding.c
+++ binding.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <bare.h>
 #include <js.h>
+#include <stdbool.h>
 
 static js_value_t *
 bare_inspect_get_promise_state(js_env_t *env, js_callback_info_t *info) {
@@ -13,6 +14,17 @@ bare_inspect_get_promise_state(js_env_t *env, js_callback_info_t *info) {
   assert(err == 0);
 
   assert(argc == 1);
+
+  bool is_promise;
+  err = js_is_promise(env, argv[0], &is_promise);
+  assert(err == 0);
+
+  if (!is_promise) {
+    err = js_throw_type_error(env, NULL, "Value must be a promise");
+    assert(err == 0);
+
+    return NULL;
+  }
 
   js_promise_state_t state;
   err = js_get_promise_state(env, argv[0], &state);
@@ -37,6 +49,17 @@ bare_inspect_get_promise_result(js_env_t *env, js_callback_info_t *info) {
 
   assert(argc == 1);
 
+  bool is_promise;
+  err = js_is_promise(env, argv[0], &is_promise);
+  assert(err == 0);
+
+  if (!is_promise) {
+    err = js_throw_type_error(env, NULL, "Value must be a promise");
+    assert(err == 0);
+
+    return NULL;
+  }
+
   js_value_t *result;
   err = js_get_promise_result(env, argv[0], &result);
   assert(err == 0);
@@ -55,6 +78,17 @@ bare_inspect_get_external(js_env_t *env, js_callback_info_t *info) {
   assert(err == 0);
 
   assert(argc == 1);
+
+  bool is_external;
+  err = js_is_external(env, argv[0], &is_external);
+  assert(err == 0);
+
+  if (!is_external) {
+    err = js_throw_type_error(env, NULL, "Value must be an external");
+    assert(err == 0);
+
+    return NULL;
+  }
 
   void *data;
   err = js_get_value_external(env, argv[0], &data);
@@ -78,6 +112,17 @@ bare_inspect_get_own_non_index_property_names(js_env_t *env, js_callback_info_t 
   assert(err == 0);
 
   assert(argc == 1);
+
+  js_value_type_t type;
+  err = js_typeof(env, argv[0], &type);
+  assert(err == 0);
+
+  if (type != js_object) {
+    err = js_throw_type_error(env, NULL, "Value must be an object");
+    assert(err == 0);
+
+    return NULL;
+  }
 
   js_value_t *result;
   err = js_get_filtered_property_names(env, argv[0], js_key_own_only, js_property_only_enumerable, js_index_skip_indices, js_key_convert_to_string, &result);
